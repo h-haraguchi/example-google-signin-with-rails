edf0e827faf41ec92284a2b7fae271336193b865
h-haraguchi <account.cgtdnpck@me.com>
2018-11-13 23:59:46 +0900

    create hello page and let it root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signin', controller: :sign_in, action: :show
+  root 'sign_in#show'
 end
