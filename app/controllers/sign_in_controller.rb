658df15a70b78a76d9326d4de33a8bcb80bbc5ad
h-haraguchi <account.cgtdnpck@me.com>
2018-11-17 03:17:39 +0900

    suppress warning

--- app/controllers/sign_in_controller.rb
+++ app/controllers/sign_in_controller.rb
@@ -7,7 +7,7 @@ class SignInController < ApplicationController
   def tokensignin
     require 'google/apis/oauth2_v2'
     service = Google::Apis::Oauth2V2::Oauth2Service.new
-    credential = service.tokeninfo(id_token: params[:idtoken])
+    credential = service.tokeninfo(id_token: params[:id_token])
     puts '#############################'
     pp credential
     puts '#############################'
