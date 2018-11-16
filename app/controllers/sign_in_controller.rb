af8b85d3b951f4f6ec1894d9bf95b136a93dc7e6
h-haraguchi <account.cgtdnpck@me.com>
2018-11-17 03:02:49 +0900

    add callback function and make sign button toggle

--- app/controllers/sign_in_controller.rb
+++ app/controllers/sign_in_controller.rb
@@ -2,6 +2,15 @@ class SignInController < ApplicationController
 
   layout false
 
-  def show
+  def show; end
+
+  def tokensignin
+    require 'google/apis/oauth2_v2'
+    service = Google::Apis::Oauth2V2::Oauth2Service.new
+    credential = service.tokeninfo(id_token: params[:idtoken])
+    puts '#############################'
+    pp credential
+    puts '#############################'
+    render body: 'OK'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/signin', controller: :sign_in, action: :show
   root 'sign_in#show'
+  post '/tokensignin', controller: :sign_in, action: :tokensignin
 end
